80d07327849596f69a244134dcc5a6ebbefdbe69
muhammad tanko <teekay014@gmail.com>
2025-12-11 09:49:59 +0100

    minor changes3

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
-const { APP_PORT, MONGO_URI } = process.env;
 module.exports = {
-    appPort: APP_PORT || 3000,
-    mongoURI: MONGO_URI ||"mongodb+srv://muhammad:$ecretPass1@library.v1cau.mongodb.net/?retryWrites=true&w=majority&appName=library",
+    appPort: 3000,
+    mongoURI: "mongodb+srv://muhammad:$ecretPass1@library.v1cau.mongodb.net/?retryWrites=true&w=majority&appName=library",
     // mongoURI: MONGO_URI ||"mongodb+srv://muhammad:$ecretPass1@library.v1cau.mongodb.net/?retryWrites=true&w=majority&appName=library",
 }
 
